3b57c487695573aa43a8a4ad0eec8ef294f136ee
Arunjith <arunjith.unofficial@gmail.com>
2025-01-12 23:22:06 +0530

    added code to calculate the dimensions of the table

--- src/main/java/com/sicxbit/utils/CSVFileReader.java
+++ src/main/java/com/sicxbit/utils/CSVFileReader.java
@@ -10,9 +10,17 @@ public class CSVFileReader {
     public void readCSV(String filepath) throws CsvValidationException, IOException {
         try (CSVReader reader = new CSVReader(new FileReader(filepath))) {
             String[] line;
-            while ((line = reader.readNext()) != null) {
-                System.out.println(String.join(",", line));
+            int rowCount = 0;
+            String[] headers = reader.readNext();
+            if (headers != null) {
+                System.out.println("Columns: "+ String.join(",", headers));
             }
+            while ((line = reader.readNext()) != null) {
+                rowCount++;
+                String.join(",", line);
+            }
+
+            System.out.println("Total rows: "+ rowCount+ ", Total columns: "+ headers.length );
         } catch (CsvValidationException | IOException e) {
             e.printStackTrace();
         }
